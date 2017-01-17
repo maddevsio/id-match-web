f68f5e96e6ef5ace12bba1e5cb31f06246aa37ec
puzanov <puzanov@gmail.com>
2017-01-17 13:24:26 +0600

    * file upload is done. ready to send photos to "idmatchd"

--- src/routes.php
+++ src/routes.php
@@ -1,18 +1,43 @@
 <?php
-// Routes
 
 $app->get('/', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
 $app->post('/', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    $files = $request->getUploadedFiles();
+    if (empty($files['face'])) {
+        throw new Exception('Expected a face');
+    }
+    if (empty($files['id'])) {
+        throw new Exception('Expected an id');
+    }
 
-    // Render index view
+    $faceFilePath = '';
+    $idFilePath = '';
+
+    if ($files['face']->getError() === UPLOAD_ERR_OK) {
+        $uploadFileName = $files['face']->getClientFilename();
+        $rand = md5(microtime(true));
+        $faceFilePath = "/tmp/{$rand}-{$uploadFileName}";
+        $files['face']->moveTo($faceFilePath);
+    }
+
+    if ($files['id']->getError() === UPLOAD_ERR_OK) {
+        $uploadFileName = $files['id']->getClientFilename();
+        $rand = md5(microtime(true));
+        $idFilePath = "/tmp/{$rand}-{$uploadFileName}";
+        $files['id']->moveTo($idFilePath);
+    }
+
+    if (empty($faceFilePath)) {
+        throw new Exception('Вы не зааплоадили лицо');
+    }
+    if (empty($idFilePath)) {
+        throw new Exception('Вы не зааплоадили айдишку');
+    }
+
+
+
     return $this->renderer->render($response, 'index.phtml', $args);
 });
