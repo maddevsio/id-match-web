6f187a370f6c9bab18268fb4641cf47e0fd8abbb
M1rotvorez <volkik91@gmail.com>
2017-02-10 17:24:41 +0600

    Add new header / Animated console / add JQ, Typed scripts /

---
+++ public/js/scripts.js
@@ -0,0 +1,95 @@
+$(function () {
+    $("#typed").typed({
+        stringsElement: $('#typed-strings'),
+        typeSpeed: -20,
+        startDelay: 2,
+        cursorChar: "&block;",
+        callback: function() {
+            $('.typed-cursor').hide();
+            var count = 15;
+            var dots = $('#dots');
+            var timer = setInterval(
+                function(){
+                    dots.text(dots.text() + '.');
+                    count --;
+                    if(count == 0){
+                        clearInterval(timer);
+                        $('.json_wrap').show();
+                        $('.inst-info').fadeIn('slow');
+                    }
+                }, 100
+            );
+        }
+    });
+});
+
+
+$(function () {
+    var width = 320;
+    var height = 0;
+
+    var streaming = false;
+
+    var video = null;
+    var canvas = null;
+    var photo = null;
+    var startbutton = null;
+
+
+    // $(function startup() {
+    //     video = document.getElementById('video');
+    //     canvas = document.getElementById('canvas');
+    //     photo = document.getElementById('photo');
+    //     startbutton = document.getElementById('startbutton');
+    //
+    //     navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    //         .then(function (stream) {
+    //             video.srcObject = stream;
+    //             video.play();
+    //         })
+    //         .catch(function (err) {
+    //             console.log("An error occured! " + err);
+    //         });
+    //
+    //     video.addEventListener('canplay', function (ev) {
+    //         if (!streaming) {
+    //             height = video.videoHeight / (video.videoWidth / width);
+    //
+    //             video.setAttribute('width', width);
+    //             video.setAttribute('height', height);
+    //             canvas.setAttribute('width', width);
+    //             canvas.setAttribute('height', height);
+    //             streaming = true;
+    //         }
+    //     }, false);
+    //
+    //     startbutton.addEventListener('click', function (ev) {
+    //         takepicture();
+    //         ev.preventDefault();
+    //     }, false);
+    //
+    //     function clearphoto() {
+    //         var context = canvas.getContext('2d');
+    //         context.fillStyle = "#AAA";
+    //         context.fillRect(0, 0, canvas.width, canvas.height);
+    //
+    //         var data = canvas.toDataURL('image/png');
+    //         photo.css( "background" , data);
+    //     }
+    //
+    //     function takepicture() {
+    //         var context = canvas.getContext('2d');
+    //         if (width && height) {
+    //             canvas.width = width;
+    //             canvas.height = height;
+    //             context.drawImage(video, 0, 0, width, height);
+    //
+    //             var data = canvas.toDataURL('image/png');
+    //             $(photo).css('background-image', 'url(' + data + ')');
+    //             console.log(data)
+    //         } else {
+    //             clearphoto();
+    //         }
+    //     }
+    // });
+});
