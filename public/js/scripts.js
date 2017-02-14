8b39f8ec236077552cd2864abc4c0ba700dd2ae8
M1rotvorez <volkik91@gmail.com>
2017-02-14 18:55:19 +0600

    Add new

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -35,62 +35,62 @@ $(function () {
     var startbutton = null;
 
 
-    // $(function startup() {
-    //     video = document.getElementById('video');
-    //     canvas = document.getElementById('canvas');
-    //     photo = document.getElementById('photo');
-    //     startbutton = document.getElementById('startbutton');
-    //
-    //     navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    //         .then(function (stream) {
-    //             video.srcObject = stream;
-    //             video.play();
-    //         })
-    //         .catch(function (err) {
-    //             console.log("An error occured! " + err);
-    //         });
-    //
-    //     video.addEventListener('canplay', function (ev) {
-    //         if (!streaming) {
-    //             height = video.videoHeight / (video.videoWidth / width);
-    //
-    //             video.setAttribute('width', width);
-    //             video.setAttribute('height', height);
-    //             canvas.setAttribute('width', width);
-    //             canvas.setAttribute('height', height);
-    //             streaming = true;
-    //         }
-    //     }, false);
-    //
-    //     startbutton.addEventListener('click', function (ev) {
-    //         takepicture();
-    //         ev.preventDefault();
-    //     }, false);
-    //
-    //     function clearphoto() {
-    //         var context = canvas.getContext('2d');
-    //         context.fillStyle = "#AAA";
-    //         context.fillRect(0, 0, canvas.width, canvas.height);
-    //
-    //         var data = canvas.toDataURL('image/png');
-    //         photo.css( "background" , data);
-    //     }
-    //
-    //     function takepicture() {
-    //         var context = canvas.getContext('2d');
-    //         if (width && height) {
-    //             canvas.width = width;
-    //             canvas.height = height;
-    //             context.drawImage(video, 0, 0, width, height);
-    //
-    //             var data = canvas.toDataURL('image/png');
-    //             $(photo).css('background-image', 'url(' + data + ')');
-    //             console.log(data)
-    //         } else {
-    //             clearphoto();
-    //         }
-    //     }
-    // });
+    $(function startup() {
+        video = document.getElementById('video');
+        canvas = document.getElementById('canvas');
+        photo = document.getElementById('photo');
+        startbutton = document.getElementById('startbutton');
+
+        navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function (err) {
+                console.log("An error occured! " + err);
+            });
+
+        video.addEventListener('canplay', function (ev) {
+            if (!streaming) {
+                height = video.videoHeight / (video.videoWidth / width);
+
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                streaming = true;
+            }
+        }, false);
+
+        startbutton.addEventListener('click', function (ev) {
+            takepicture();
+            ev.preventDefault();
+        }, false);
+
+        function clearphoto() {
+            var context = canvas.getContext('2d');
+            context.fillStyle = "#AAA";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            var data = canvas.toDataURL('image/png');
+            photo.css( "background" , data);
+        }
+
+        function takepicture() {
+            var context = canvas.getContext('2d');
+            if (width && height) {
+                canvas.width = width;
+                canvas.height = height;
+                context.drawImage(video, 0, 0, width, height);
+
+                var data = canvas.toDataURL('image/png');
+                $(photo).css('background-image', 'url(' + data + ')');
+                console.log(data)
+            } else {
+                clearphoto();
+            }
+        }
+    });
 });
 
 $(function () {
